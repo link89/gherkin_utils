416ce4716f8df2fb3575228b2c043c48708e5e89
Henry Xu <henry.xu@ringcentral.com>
2018-02-27 17:00:48 +0800

    chore: setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from distutils.core import setup
+setup(name='gherkin_utils',
+      packages=['gherkin_utils.tools'],
+      version='0.0.1-snapshot',
+      description='Gherkin Utils',
+      author='Henry Xu',
+      url='https://github.com/link89/gherkin_utils',
+      license='MIT',
+      keywords=['gherkin', 'bdd'],
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3',
+                   ],
+      )
