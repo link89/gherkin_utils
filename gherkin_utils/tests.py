2ab9156885d55cd92b515224f31c0796e282de83
Henry Xu <henry.xu@ringcentral.com>
2018-02-27 16:55:00 +0800

    chore: modify structure for release

--- gherkin_utils/tests.py
+++ gherkin_utils/tests.py
@@ -6,15 +6,6 @@ from tools import BuildIndexTask, MetaUtils
 from tools import new_uuid_80b, new_uuid_120b
 
 
-class TestBuildIndex(TestCase):
-    def test_build_index(self):
-        task = BuildIndexTask(path='/tmp/test_repo',
-                              url='https://github.com/cucumber/gherkin-python.git',
-                              branches=('master', 'dev', 'lalala'),
-                              )
-        task.run()
-
-
 class TestMetaUtils(TestCase):
     def test_new_scenario_meta_pattern(self):
         fuid = 'F' * 16
@@ -31,13 +22,3 @@ class TestMetaUtils(TestCase):
         s_meta_line = MetaUtils.new_scenario_meta(fuid, suid, 54321)
         self.assertEqual(MetaUtils.split_feature_meta(f_meta_line), (fuid, 12345))
         self.assertEqual(MetaUtils.split_scenario_meta(s_meta_line), (fuid, suid, 54321))
-
-
-class TestUtils(TestCase):
-    def test_new_uuid(self):
-        for _ in range(1000):
-            uuid = new_uuid_80b()
-            print(uuid)
-        for _ in range(1000):
-            uuid = new_uuid_120b()
-            print(uuid)
